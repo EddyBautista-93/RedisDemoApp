75ba59284682f24a863219b9effa9bbaea92465a
EddyBautista <bautista.j.eddy@gmail.com>
2021-10-19 20:14:01 -0500

    add connection string, create new holder to hold extension methods ( SetRecordAsync, GetRecordAsync)

---
+++ RedisDemo/Extensions/DistributedCacheExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisDemo.Extensions
+{
+    public static class DistributedCacheExtensions
+    {
+        // ? at the end means nullable 
+        public static async Task SetRecordAsync<T>(this IDistributedCache cache, string recordId, T data, TimeSpan? absoluteExpireTime = null, TimeSpan? unusedExpireTime = null)
+        {
+            var options = new DistributedCacheEntryOptions();
+
+            // if we don't set experation time we will default it to 60 seconds. 
+            options.AbsoluteExpirationRelativeToNow = absoluteExpireTime ?? TimeSpan.FromSeconds(60);
+            options.SlidingExpiration = unusedExpireTime;
+
+            var jsonData = JsonSerializer.Serialize(data);
+            // saving the data 
+            await cache.SetStringAsync(recordId, jsonData, options); 
+        }
+
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cache, string recordId)
+        {
+            var jsonData = await cache.GetStringAsync(recordId);
+
+            if (jsonData is null)
+            {
+                return default(T);
+            }
+
+            return JsonSerializer.Deserialize<T>(jsonData);
+
+        }
+
+    }
+}
